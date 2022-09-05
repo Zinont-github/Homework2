e4d03ad1a7071361d7e0aa35e7f2d3e039cfb652
Zinont <zinont@bk.ru>
2022-09-05 21:10:10 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу которая принимает трехзначное число и выводит вторую цифру числа
+
+int number = ReadInt("Введите трехзначное число: ");
+Console.WriteLine(number);
+Console.WriteLine(DeleteFirstThridDigit(number));
+
+
+int DeleteFirstThridDigit(int secondDigit)
+{
+    int a = number % 100;
+    int b = a / 10;
+    return b;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
