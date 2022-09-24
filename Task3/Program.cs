b7b1ac84c7e9c97a1c29d328cce7fa091da6ac35
Zinont <zinont@bk.ru>
2022-09-24 18:32:18 +0400

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = ReadInt("Введите число, обозначающее день недели: ");
+if(number == 6 || number == 7)
+{
+    Console.WriteLine("Это выходной!");
+}
+else if(number < 1 || number > 7)
+{
+    Console.WriteLine("Это не день недели!");
+}
+else
+{
+    Console.WriteLine("Это не выходной день!");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
